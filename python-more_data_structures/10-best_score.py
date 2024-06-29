847df61b17869bfd9bc07a27806425cd9a72faee
JoanK7 <j.kariza@alustudent.com>
2024-06-29 10:58:32 -0700

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,13 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
+
     max_value = float('-inf')
     best_key = None
-    
+
     for key, value in a_dictionary.items():
         if value > max_value:
             max_value = value
             best_key = key
-    
+
     return best_key
