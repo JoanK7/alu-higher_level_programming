993e375740608596276ae9b2257fe4ad3175c61c
JoanK7 <j.kariza@alustudent.com>
2024-07-16 09:54:43 -0700

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''function definition'''
+
+def read_file(filename=""):
+    '''read file function'''
+
+    with open(filename, 'r',encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
