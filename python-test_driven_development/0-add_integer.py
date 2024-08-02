1de033f09faa35af0936d1738ac75771e1b6b402
JoanK7 <j.kariza@alustudent.com>
2024-08-02 08:46:10 -0700

    Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines a function that adds 2 integers."""
+
+def add_integer(a, b=98):
+    """"Returns an integer: the addition of a and b
+    a and b must be first casted to integers if they are float
+    Raise a TypeError exception if
+    a and b are not integers or floats."""
+
+      if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
