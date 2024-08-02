2d142eddd1bdaf75d9edb72935f35080170bb115
JoanK7 <j.kariza@alustudent.com>
2024-08-01 23:26:30 -0700

    Error code

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -2,7 +2,7 @@
 """Script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 
-manages urllib.error.HTTPError exceptions and 
+manages urllib.error.HTTPError exceptions and
 print: Error code: followed by the HTTP status code
 """
 
